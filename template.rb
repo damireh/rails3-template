0b834f40e20effbe2e4efea3f51c5987fd12a826
Daniel Amireh <daniel@amireh.org>
2012-03-16 18:14:45 +0200

    create rvmrc with default ruby and new gemset with app name

--- template.rb
+++ template.rb
@@ -9,3 +9,7 @@ end
 
 require "colored"
 require "bundler"
+
+# create rvmrc
+puts "Creating rvmrc with default ruby using gemset #{app_name}".black_on_green
+create_file '.rvmrc', 'rvm gemset default@#{app_name}'
