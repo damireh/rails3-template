0fe09885d643e76916a3a7cdaa2d46cef3f73914
Daniel Amireh <daniel@amireh.org>
2012-03-16 18:16:59 +0200

    removed rails default files

--- template.rb
+++ template.rb
@@ -13,3 +13,8 @@ require "bundler"
 # create rvmrc
 puts "Creating rvmrc with default ruby using gemset #{app_name}".black_on_green
 create_file '.rvmrc', 'rvm gemset default@#{app_name}'
+
+# remove rails default files
+puts "Removing rails default files".white_on_red
+remove_file 'public/index.html'
+remove_file 'app/assets/images/rails.png'
