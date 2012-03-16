171411bd392a3ef22be088b3c860d9164a8b0592
Daniel Amireh <daniel@amireh.org>
2012-03-16 18:24:10 +0200

    added frequently used gems

--- template.rb
+++ template.rb
@@ -18,3 +18,28 @@ create_file '.rvmrc', 'rvm gemset default@#{app_name}'
 puts "Removing rails default files".white_on_red
 remove_file 'public/index.html'
 remove_file 'app/assets/images/rails.png'
+
+# add most used gems
+puts "Adding frequently used Gems (Devise, CanCan, Thin, Debugger, Minitest)".white_on_blue
+gem 'thin'
+gem 'devise'
+gem 'cancan'
+
+gem_group :development do
+  gem 'wirb'
+
+  # debugger
+  # see https://gist.github.com/1333785
+  gem 'linecache19', '0.5.13'
+  gem 'ruby-debug-base19', '0.11.26'
+  gem 'ruby-debug19', :require => 'ruby-debug'
+end
+
+gem_group :test do
+  gem 'factory_girl_rails'
+  gem 'minitest'
+  gem 'turn'
+end
+
+# bundle
+run 'bundle install'
