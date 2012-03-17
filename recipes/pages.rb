d07b947c0a667c3d68b33162744dd902111d0586
Daniel Amireh <daniel@amireh.org>
2012-03-17 14:49:49 +0200

    remove unused js, css and helpers

--- recipes/pages.rb
+++ recipes/pages.rb
@@ -1,6 +1,9 @@
 # add basic welcome page and set it as root
 generate "controller", "welcome index"
 route("root to: 'welcome#index'")
+remove_file "app/assets/stylesheets/welcome.css.scss"
+remove_file "app/assets/javascripts/welcome.js.coffee"
+remove_file "app/helpers/welcome_helper.rb"
 
 git add: '.'
 git commit: "-am 'generated welcome controller and set root route to index action'"
