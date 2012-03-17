4aae1fcd2402a7eff440d6c02482cf2119a597a3
Daniel Amireh <daniel@amireh.org>
2012-03-17 11:46:46 +0200

    moved gem installation to partial

--- template.rb
+++ template.rb
@@ -21,50 +21,16 @@ def copy_static_file(path)
   create_file path, File.read(File.join(@static_files, path))
 end
 
-apply "{@partials}/_rvm.rb"
+apply "#{@partials}/_rvm.rb"
 
 # remove rails default files
 puts "Removing rails default files".white_on_red
 remove_file 'public/index.html'
 remove_file 'app/assets/images/rails.png'
 
-# add twitter bootstrap and h5bp
-puts "Adding Twitter Bootstrap and HTML5 Boilerplate files"
-copy_static_file "vendor/assets/stylesheets/bootstrap-responsive.min.css"
-copy_static_file "vendor/assets/stylesheets/bootstrap.min.css"
-copy_static_file "vendor/assets/javascripts/bootstrap.min.js"
-copy_static_file "vendor/assets/images/glyphicons-halflings-white.png"
-copy_static_file "vendor/assets/images/glyphicons-halflings.png"
+apply "#{@partials}/_bootstrap.rb"
+apply "#{@partials}/_gems.rb"
 
-copy_static_file "public/apple-touch-icon.png"
-copy_static_file "public/favicon.ico"
-copy_static_file "public/humans.txt"
-copy_static_file "vendor/assets/javascripts/modernizr-2.5.3.min.js"
-
-# add most used gems
-puts "Installing frequently used Gems (Devise, CanCan, Thin, Debugger, Minitest)".white_on_blue
-gem 'thin'
-gem 'devise'
-gem 'cancan'
-
-gem_group :development do
-  gem 'wirb'
-
-  # debugger
-  # see https://gist.github.com/1333785
-  gem 'linecache19', '0.5.13'
-  gem 'ruby-debug-base19', '0.11.26'
-  gem 'ruby-debug19', :require => 'ruby-debug'
-end
-
-gem_group :test do
-  gem 'factory_girl_rails'
-  gem 'minitest'
-  gem 'turn'
-end
-
-# bundle
-run 'bundle install'
 
 # setup test environment
 puts "Seting up test environment".white_on_magenta
