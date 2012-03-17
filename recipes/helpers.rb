760ddd881434f860afc7604a66a60b3d5fd0df35
Daniel Amireh <daniel@amireh.org>
2012-03-17 16:32:59 +0200

    added helpers recipe

---
+++ recipes/helpers.rb
@@ -0,0 +1,13 @@
+# add useful helpers
+append_to_file 'app/helpers/application_helper.rb' do
+  %{def nav_link_to(text, path)
+  content_tag :li, class: (current_page?(path) ? "active" : nil) do
+    link_to text, path
+  end
+end}
+end
+
+# add bootstrap themed partials
+empty_directory "app/views/shared"
+copy_static_file "app/views/shared/_alert.html.erb"
+copy_static_file "app/views/shared/_form_error_messages.html.erb"
