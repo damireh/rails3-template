138ead2205a0910b8d8b6197affd95221c67728c
Daniel Amireh <daniel@amireh.org>
2012-06-22 19:40:17 +0300

    added unique index to user slugs

--- recipes/friendlyid.rb
+++ recipes/friendlyid.rb
@@ -1,4 +1,3 @@
 puts "Installing FriendlyId".white_on_blue
-generate 'migration add_slug_to_users slug:string'
-# TODO: add unique index on slug
+generate 'migration add_slug_to_users slug:uniq'
 run 'rake db:migrate'
