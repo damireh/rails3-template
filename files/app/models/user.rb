f618dd10f72334ba4f4b00e74efc18e0463eaf48
Daniel Amireh <daniel@amireh.org>
2012-07-21 23:10:07 +0300

    Ruby 1.9 hash syntax

--- files/app/models/user.rb
+++ files/app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     login = conditions.delete(:login)
-    where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.strip.downcase }]).first
+    where(conditions).where(["lower(username) = :value OR lower(email) = :value", { value: login.strip.downcase }]).first
   end
 end
 
