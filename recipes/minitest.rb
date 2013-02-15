786ffa74980c8c71b8440da5cc951dfe3d211c43
Daniel Amireh <daniel@amireh.org>
2013-02-15 17:35:10 +0200

    update minitest recipe
    removed turn
    used minitest-rails gem isntead of minitest
    added both unit/acceptance tests to rake stats

--- recipes/minitest.rb
+++ recipes/minitest.rb
@@ -1,29 +1,14 @@
 # setup test environment
 puts "Seting up test environment".white_on_magenta
-empty_directory 'test'
-
-create_file 'test/minitest_helper.rb' do
-  %{ENV['RAILS_ENV'] = 'test'
-require File.expand_path('../../config/environment', __FILE__)
-require 'minitest/autorun'
-
-Turn.config.format = :cue
-Turn.config.natural = true}
-end
-
-rakefile 'minitest.rake' do
-  %{require 'rake/testtask'
-
-Rake::TestTask.new(test: 'db:test:prepare') do |t|
-  t.libs << 'test'
-  t.pattern = 'test/**/*_test.rb'
-end}
-end
+generate 'mini_test:install'
+empty_directory 'test/models'
+empty_directory 'test/acceptance'
 
 append_to_file 'Rakefile' do
   %{namespace :spec do
   task :statsetup do
-    ::STATS_DIRECTORIES << ['Unit tests',  'test/models']
+    ::STATS_DIRECTORIES << ['Unit Tests',  'test/models']
+    ::STATS_DIRECTORIES << ['Acceptance Tests',  'test/acceptance']
   end
 end
 
